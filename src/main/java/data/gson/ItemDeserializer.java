b3632d655d874622a0d6220963cc72544e309195
dobrin_r <rashkov09@gmail.com>
2023-10-26 18:39:43 +0300

    IMS-11 added clothing item category

--- src/main/java/data/gson/ItemDeserializer.java
+++ src/main/java/data/gson/ItemDeserializer.java
@@ -5,9 +5,13 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import model.enums.ClothingSexCategory;
+import model.enums.ClothingSize;
+import model.enums.ClothingType;
 import model.enums.ElectronicsType;
 import model.enums.GroceryType;
 import model.enums.ItemCategory;
+import model.impl.ClothingItem;
 import model.impl.ElectronicsItem;
 import model.impl.GroceryItem;
 import model.impl.InventoryItem;
@@ -50,6 +54,12 @@ public class ItemDeserializer implements JsonDeserializer<InventoryItem> {
 				LocalDate expirationDate = DateParser.parseFromString(jsonObject.get("expirationDate").getAsString());
 				return new GroceryItem(id, name, manufacturer, country, description, category, price, quantity, groceryType,
 				                       expirationDate);
+			}
+			case CLOTHING_APPAREL -> {
+				ClothingType clothingType = ClothingType.valueOf(jsonObject.get("clothingType").getAsString());
+				ClothingSexCategory clothingSexCategory = ClothingSexCategory.valueOf(jsonObject.get("clothingSexCategory").getAsString());
+				ClothingSize clothingSize = ClothingSize.valueOf(jsonObject.get("clothingSize").getAsString());
+				return  new ClothingItem(id, name, manufacturer,country,description,category,price,quantity,clothingType,clothingSexCategory,clothingSize);
 			}
 			default -> throw new JsonParseException("Error");
 		}
